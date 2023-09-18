99f11ce2d5c6e961a3983bae0373536c6faf73c0
Rizwan <mu.rizwan815@gmail.com>
2023-09-18 13:19:29 +0500

    Basic vite Project

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+      <h1>Chai with React</h1>
+      <Chai/>
+      <p>This is Paragraph</p>
+    </>
+  )
+}
+
+export default App
